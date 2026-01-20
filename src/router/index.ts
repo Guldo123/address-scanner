eb1b5c3636dbbf9d0cd86841b98519192859b5e7
Guldo123 <Markus.Kaufmann1@gmx.ch>
2026-01-20 15:38:45 +0100

    Updated vite.config.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Scanner from '../pages/Scanner.vue'
 import History from '../pages/History.vue'
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,16 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'X-Frame-Options': 'ALLOWALL'
+    }
+  },
+  preview: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'X-Frame-Options': 'ALLOWALL'
+    }
   }
 })
